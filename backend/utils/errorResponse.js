9351ed6e9699fdb53a910bc98e6ecacec19cea4a
SULABH11 <2020uch0004@iitjammu.ac.in>
2023-06-04 13:39:00 +0530

    error resolved

---
+++ backend/utils/errorResponse.js
@@ -0,0 +1,9 @@
+/*jshint esversion: 6 */
+class ErrorResponse extends Error {
+    constructor(message, codeStatus) {
+        super(message);
+        this.codeStatus = codeStatus;
+    }
+}
+
+module.exports = ErrorResponse;
